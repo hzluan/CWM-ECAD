020a54d90c140ac00a93b0beb2493524f40e7c3e
hzluan <66193810+hzluan@users.noreply.github.com>
2020-06-02 17:54:38 +0800

    Update top.v

--- Ex3/top.v
+++ Ex3/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #3 
-// Student Name:
-// Date: 
+// Student Name:Hongzhou Luan
+// Date: 2/June
 //
 //  Description: In this exercise, you need to design an up / down counter, where 
 //  if the rst=1, the counter should be set to zero. If enable=0, the value
