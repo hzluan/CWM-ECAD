21c2f4d5767b4908678c0e4ebc8d8bda5c03b8f6
hzluan <66193810+hzluan@users.noreply.github.com>
2020-06-03 13:04:58 +0800

    Update top_tb.v

--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -1,9 +1,66 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #4 - Electronic Dice
-// Student Name:
-// Date: 
+// Student Name:Hongzhou Luan
+// Date: 3/June
 //
 // Description: A testbench module to test Ex4 - Electronic Dice
-// You need to write the whole file
+`timescale 1ns / 100ps
+module top_tb();
+  
+  parameter CLK_PERIOD = 10;
+  
+  reg clk;
+  reg rst;
+  reg direction;
+  reg enable;
+  reg error;
+  reg [7:0]maxcount;
+  
+  
+  initial begin
+    clk = 1'b0;
+    forever begin
+      #(CLK_PERIOD/2) clk = ~clk;
+    end
+  end
+  
+  initial begin
+    rst = 0;
+    direction = 1;
+    enable = 1;
+    err = 0;
+    
+    #10
+    maxcount <= counter_out;
+    direction <= 0;
+    
+    #20
+    if(maxcount< counter_out)
+    error = 1;
+    enable = 0;
+    
+    #30
+    rst = 1;
+    
+  end
+  
+  initial begin
+    #40
+    if(!error)
+      $display("Passed");
+    $finish;
+    else $display("Failed");
+    $finish;
+  end
+  
+  dice dice(.clk(clk),
+            .rst(rst),
+            .direction(direction),
+            .enable(enable)
+               );
+  
+endmodule
+              
+
 //////////////////////////////////////////////////////////////////////////////////
 
