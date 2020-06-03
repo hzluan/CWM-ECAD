94c7870990e507e1cec28367871efd440863b583
hzluan <66193810+hzluan@users.noreply.github.com>
2020-06-03 13:24:45 +0800

    Update top.v

--- Ex5/top.v
+++ Ex5/top.v
@@ -1,7 +1,9 @@
-//////////////////////////////////////////////////////////////////////////////////
+0;
+
+inpu//////////////////////////////////////////////////////////////////////////////////
 // Exercise #5 
-// Student Name:
-// Date: 
+// Student Name:Hongzhou Luan
+// Date: 3/June
 //
 //  Description: In this exercise, you need to implement a UK traffic lights 
 //  sequencing system. 
@@ -11,5 +13,18 @@
 //
 //  outputs:
 //           red, amber, green
+
+//this traffic light is a 4-bit counter: 00(red) - 01(red + amber) - 10(green) - 11(amber)
+
+`timescale 1ns = 100ps
+
+module traffic_light(clk,red,amber,green);
+  
+  input clk,;
+  out put reg red,amber,green;
+  
+  always@(posedge clk) begin
+    red 
+    amber 
 //////////////////////////////////////////////////////////////////////////////////
 
