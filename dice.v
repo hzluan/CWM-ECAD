a174fea418630ccb2213cec92efef713cb3facbd
hzluan <66193810+hzluan@users.noreply.github.com>
2020-06-04 12:54:13 +0800

    Create dice.v

---
+++ dice.v
@@ -0,0 +1,20 @@
+module roll(clk, rst, button, throw)
+	input clk, 
+        input rst, 
+        input button;
+	output reg [2:0] throw;
+	
+	
+always @ (posedge clk or rst)
+if (rst) 
+    throw = 3'd0;
+else
+	
+    if(throw = 3'd0||throw = 3'd7||throw = 3'd6 && button)
+        throw <= 3'd1;
+    else
+	    
+        if (button or &(~throw) or &throw)
+            throw <= throw + 1;
+		
+endmodule
