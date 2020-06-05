8344745d7077b165520e8074ebd8cb3397470e88
hzluan <66193810+hzluan@users.noreply.github.com>
2020-06-05 13:45:34 +0800

    Update top.v

--- Ex7/top.v
+++ Ex7/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #7
-// Student Name:
-// Date: 
+// Student Name: Hongzhou Luan
+// Date: 5/June
 //
 //  Description: In this exercise, you need to implement a times table of 0..7x0..7
 //  using a memory.
@@ -13,4 +13,25 @@
 //           result[4:0]
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
 
+module multiplier(clk,a,b,read)
+  
+  input clk,read;
+  input [2:0]a,b;
+  output reg [4:0]result;
+  
+  wire [5:0]address;
+  
+  always@(a||b)
+    address = (a << 3) + b;
+  
+  multiplier_memory mem(
+	.clk(clk),
+	.enable(read),
+	.address(addr),
+	.result(result)
+	);
+  
+endmodule
+  
